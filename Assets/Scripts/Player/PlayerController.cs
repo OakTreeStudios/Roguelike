56040330981bacb4f03fad004c64088937233a61
Stanley Hale <halesta@oregonstate.edu>
2024-07-19 11:21:58 -0700

    Implemented Dawnosaur's Forces System

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEditor.Callbacks;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 /*
 * This script is used to control the player's actions/inputs
@@ -16,12 +17,16 @@ public class PlayerController : MonoBehaviour
 
     
     //Public script variables
-    public float speed = 5.0f;
+    public float movementSpeed  = 5.0f;
+    public float acceleration   = 1.0f;
+    public float deceleration   = 1.0f;
+    public float  velocityPower  = 2.0f;
+    
     //public float jumpForce = 5.0f;
     public Rigidbody2D playerRb;
 
     //Private script variabes
-    private Vector2 playerMovement;
+    private Vector2 inputMovement;
     
 
     // Start is called before the first rendered frame update
@@ -34,7 +39,7 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         //Get our player's inputs
-        playerMovement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")); 
+        inputMovement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
  
     }
 
@@ -45,8 +50,30 @@ public class PlayerController : MonoBehaviour
 
     //Function for managing player movement
     private void MovePlayer() {
+        //Horizontal movements
+        PlayerRun();
+
+        //Vertical movements
+    }
+
+    private void PlayerRun() {
+        //Following Dawnosaur's calculations for more responsive physics movement: https://www.youtube.com/watch?v=KbtcEVCM7bw
+
+        //Calculate our intended velocity
+        float targetSpeed = inputMovement.x * movementSpeed;
+
+        //Calculate the difference in our current velocity and desired velocity
+        float speedDifference = targetSpeed - playerRb.velocity.x;
+
+        //Find our acceleration rate depending on the situation (Accelerating or decelerating)
+        float accelerationRate = (Mathf.Abs(targetSpeed) > 0.01f) ? acceleration : deceleration;
+
+        //Applies acceleration to speed difference, then raises to a set power so acceleration increases with higher speeds
+        //Reapply the velocities direction using sign
+        float movement = Mathf.Pow(Mathf.Abs(speedDifference) * accelerationRate, velocityPower) * Mathf.Sign(speedDifference);
+
         //Add movement forces to player rigidbody
-        playerRb.AddForce(playerMovement * speed);
+        playerRb.AddForce(movement * Vector2.right);
     }
 
 }
