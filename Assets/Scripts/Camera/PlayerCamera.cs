050b2680d995fcf30ab25eb5cfdeeb6c1b0522b6
Stanley Hale <halesta@oregonstate.edu>
2024-07-28 13:39:01 -0700

    Added horizontal camera look ahead

--- Assets/Scripts/Camera/PlayerCamera.cs
+++ Assets/Scripts/Camera/PlayerCamera.cs
@@ -7,7 +7,9 @@ public class PlayerCamera : MonoBehaviour
 
     #region Camera Variables
     public Transform player;
-    public float cameraSpeed = 0.1f;
+    public float cameraSpeed = 4.0f;
+    public float lookAheadDistance = 5.0f;
+    [Range(0.01f, 1.0f)] public float lookAheadLerp = 0.8f;
     private Vector3 targetPoint = Vector3.zero;
 
     #endregion
@@ -26,6 +28,12 @@ public class PlayerCamera : MonoBehaviour
         //Update the target point position
         targetPoint = player.position;
         targetPoint.z = 0;
+
+        //Use the players's input to gauge which direction we are moving in
+        float moveDirection = Input.GetAxis("Horizontal");
+
+        //Now we want to add the look ahead distance to the target point based on the direction we are moving
+        targetPoint = Vector3.Lerp( targetPoint, new Vector3( targetPoint.x + ( lookAheadDistance * moveDirection ), targetPoint.y, targetPoint.z ), lookAheadLerp );
 
         //Set the camera's position by linearly interpolating between the two positions
         transform.position = Vector3.Lerp(transform.position, targetPoint, cameraSpeed * Time.deltaTime);
